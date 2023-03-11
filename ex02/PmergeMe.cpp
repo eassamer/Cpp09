008d25924a912ad4176386bdb709524eb583bc0f
Elmehdi Assamer <eassamer@e1r4p3.1337.ma>
2023-03-11 14:13:04 +0100

    all doneeeeee

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -100,19 +100,111 @@ int PmergeMe::check_is_num(char *str)
     }
     return 1;
 }
+
+std::list<int> PmergeMe::merge_lst(std::list<int> &left, std::list<int> &right)
+{
+  std::list<int> result;
+
+  while (!left.empty() && !right.empty()) {
+    if (left.front() <= right.front()) {
+      result.push_back(left.front());
+      left.pop_front();
+    } else {
+      result.push_back(right.front());
+      right.pop_front();
+    }
+  }
+
+  while (!left.empty()) {
+    result.push_back(left.front());
+    left.pop_front();
+  }
+
+  while (!right.empty()) {
+    result.push_back(right.front());
+    right.pop_front();
+  }
+
+  return result;
+}
+
+void PmergeMe::sort_lst(std::list<int> &list)
+{
+if (list.empty() || list.size() == 1) {
+    return;
+  }
+
+  // Split the list into two sublists
+  std::list<int> left, right;
+  size_t count = 0;
+  for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it) {
+    if (count < list.size() / 2) {
+      left.push_back(*it);
+    } else {
+      right.push_back(*it);
+    }
+    count++;
+  }
+
+  // Recursively sort the sublists
+  sort_lst(left);
+  sort_lst(right);
+
+  // Merge the sorted sublists into a single sorted list
+  list = merge_lst(left, right);
+}
+
 double PmergeMe::sorter(char **av)
 {
+    std::clock_t s, e;
+    double v_time, l_time;
     for (int i = 1; av[i]; i++)
     {
+        if (av[i][0] == '-')
+            throw std::invalid_argument("Error");
         if (check_is_num(av[i]))
         {
             vec.push_back(std::stoi(av[i]));
             lst.push_back(std::stoi(av[i]));
         }
         else
-            throw std::invalid_argument("Error: " + std::string(av[i]) + " is not a number");
+            throw std::invalid_argument("Error");
     }
+    std::cout << "Before: ";
+    if (vec.size() <= (size_t)5)
+    {
+        for (size_t i = 0; i < vec.size(); i++)
+            std::cout << vec[i] << " ";
+        std::cout << std::endl;
+    }
+    else
+    {
+        for (size_t i = 0; i < 5; i++)
+            std::cout << vec[i] << " ";
+        std::cout << "[...]" << std::endl;
+    }
+    s = std::clock();
     sort_vec(vec, 0, vec.size() - 1, vec.size() / 4);
-    for (size_t i = 0; i < vec.size(); i++)
-        std::cout << vec[i] << " ";
+    e = std::clock();
+    v_time = (double)(e - s) / CLOCKS_PER_SEC * 1000;
+    s = std::clock();
+    sort_lst(lst);
+    e = std::clock();
+    l_time = (double)(e - s) / CLOCKS_PER_SEC * 1000;
+    std::cout << "After:  ";
+    if (vec.size() <= (size_t)5)
+    {
+        for (size_t i = 0; i < vec.size(); i++)
+            std::cout << vec[i] << " ";
+        std::cout << std::endl;
+    }
+    else
+    {
+        for (size_t i = 0; i < 5; i++)
+            std::cout << vec[i] << " ";
+        std::cout << "[...]" << std::endl;
+    }
+    std::cout << "Time to process a range of " << vec.size()<< " elements with std::vector: " << (double)v_time << " us" << std::endl;
+    std::cout << "Time to process a range of " << vec.size() <<" elements with std::list: " << (double)l_time << " us" << std::endl;
+    return 0;
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <list>
+#include <ctime>
 
 class PmergeMe {
     private :
@@ -18,8 +19,14 @@ class PmergeMe {
         void print();
         double sorter(char **av);
         int check_is_num(char *str);
+        
         void sort_vec(std::vector<int> &vec, int left, int right, int k);
         void insertion_sort_vec(std::vector<int> &vec, int left, int right);
         void merge_vec(std::vector<int> &vec, int left, int mid, int right);
+
+
+        void sort_lst(std::list<int> &list);
+
+        std::list<int> merge_lst(std::list<int> &left, std::list<int> &right);
 };
 #endif
